978f160f3925b31089d82762bb3e421d1f7a7d93
Matte Noble <mnoble@newrelic.com>
2013-06-17 19:51:25 -0700

    Adding vimlink to install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 [ -e /usr/local/vimlink ] && exit 0
 git clone git://github.com/mnoble/vimlink /usr/local/vimlink
+ln -s /usr/local/vimlink/bin/vimlink /usr/local/bin/vimlink
 ln -s /usr/local/vimlink/bin/vimlink-add /usr/local/bin/vimlink-add
 ln -s /usr/local/vimlink/bin/vimlink-install /usr/local/bin/vimlink-install
