673e2a035994970ceaa1b6b9d0f97a201b200395
Matte Noble <mnoble@newrelic.com>
2013-07-06 11:30:22 -0700

    Symlink uninstall command

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ git clone git://github.com/mnoble/vimlink /usr/local/vimlink
 ln -s /usr/local/vimlink/bin/vimlink /usr/local/bin/vimlink
 ln -s /usr/local/vimlink/bin/vimlink-add /usr/local/bin/vimlink-add
 ln -s /usr/local/vimlink/bin/vimlink-install /usr/local/bin/vimlink-install
+ln -s /usr/local/vimlink/bin/vimlink-install /usr/local/bin/vimlink-uninstall
